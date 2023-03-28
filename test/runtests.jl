3ec5ce9b72ad0864161a092d82331de512155144
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-03-28 15:45:46 -0600

    rm JLArrays

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,17 +4,6 @@ using NNlib, NNlibCUDA
 
 import CUDA
 
-if CUDA.functional()
-    using CUDA  # exports CuArray, etc
-    @info "starting CUDA tests"
-else
-    @info "CUDA not functional, testing via JLArrays"
-    using JLArrays
-    JLArrays.allowscalar(false)
-    CUDA.cu(x) = jl(x)
-
-end
-
 # sanity check
 function ref_attention(Q,K,V)
     S = batched_mul(permutedims(K, (2,1,3,4)), Q)
@@ -23,11 +12,14 @@ function ref_attention(Q,K,V)
     return O
 end
 
-@testset "FlashAttention.jl" begin
-    Q = CUDA.cu(rand(3,256,4,3));
-    K = CUDA.cu(rand(3,256,4,3));
-    V = CUDA.cu(rand(3,256,4,3));
-    O = flash_attention(Q,K,V);
-    O_ref = ref_attention(Q,K,V);
-    @test O ≈ O_ref
+
+if CUDA.functional()
+    @testset "FlashAttention.jl" begin
+        Q = CUDA.cu(rand(3,256,4,3));
+        K = CUDA.cu(rand(3,256,4,3));
+        V = CUDA.cu(rand(3,256,4,3));
+        O = flash_attention(Q,K,V);
+        O_ref = ref_attention(Q,K,V);
+        @test O ≈ O_ref
+    end
 end
