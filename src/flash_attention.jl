e211949e963a4e4fcc7f7befddc73bdd519173e5
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-03-28 13:26:57 -0600

    Update flash_attention.jl

--- src/flash_attention.jl
+++ src/flash_attention.jl
@@ -52,6 +52,10 @@ function flash_attention_kernel(Q, K, V, O)
             for m in 1:d
                 k[m, tx] = K[m, K_idx, blockIdx().y, blockIdx().z]
             end
+        else
+            for m in 1:d
+                k[m, tx] = zero(T)
+            end
         end
         sync_threads()
 
@@ -73,46 +77,44 @@ function flash_attention_kernel(Q, K, V, O)
         end
 
         sync_threads()
-#=
-       # m̃ᵢⱼ = maximum(s; dims=1)
+
         l̃ᵢⱼ = zero(T)
-        for m in 1:Bs
-            P̃ᵢⱼ[m, tx] = exp(s[m, tx] - m̃ᵢⱼ)
-            l̃ᵢⱼ += P̃ᵢⱼ[m, tx]
+        for n in 1:Bs
+            tmp = exp(s[n, tx] - m̃ᵢⱼ)
+            P̃ᵢⱼ[n, tx] = tmp
+            l̃ᵢⱼ += tmp
         end
 
         mᵢⁿᵉʷ = max(mᵢ[tx], m̃ᵢⱼ)
         lᵢⁿᵉʷ = exp(mᵢ[tx]-mᵢⁿᵉʷ) * lᵢ[tx] + exp(m̃ᵢⱼ-mᵢⁿᵉʷ)*l̃ᵢⱼ
-=#
+
         # Load V to shared memory, which is same as K
         if K_idx <= NK
             for m in 1:d
                 k[m, tx] = V[m, K_idx, blockIdx().y, blockIdx().z]
             end
+        else
+            for m in 1:d
+                k[m, tx] = zero(T)
+            end
         end
 
         sync_threads()
 
-        if blockIdx().y == 1 && blockIdx().z == 1
-            @cushow j
-            @cushow k[1, tx]
-            @cushow k[2, tx]
-        end
-
-      #  w1 = lᵢ[tx] * exp(mᵢ[tx]-mᵢⁿᵉʷ)
-     #   w2 = exp(m̃ᵢⱼ-mᵢⁿᵉʷ)
+        w₁ = lᵢ[tx] * exp(mᵢ[tx]-mᵢⁿᵉʷ) / lᵢⁿᵉʷ
+        w₂ = exp(m̃ᵢⱼ-mᵢⁿᵉʷ) / lᵢⁿᵉʷ
 
         # compute V * P
         for m in 1:d
             tmp = zero(T)
             for n in 1:Bs
-                tmp += k[m, n] * s[n, tx]#P̃ᵢⱼ[n, tx]
+                tmp += k[m, n] * P̃ᵢⱼ[n, tx]
             end
-            o[m, tx] = tmp#(w1 * o[m,tx] + w2 * tmp) / lᵢⁿᵉʷ
+            o[m, tx] = w₁ * o[m,tx] + w₂ * tmp
         end
 
-       # lᵢ[tx] = lᵢⁿᵉʷ
-       # mᵢ[tx] = mᵢⁿᵉʷ
+        lᵢ[tx] = lᵢⁿᵉʷ
+        mᵢ[tx] = mᵢⁿᵉʷ
         sync_threads()
     end
 
@@ -128,17 +130,27 @@ end
 
 
 function flash_attention(Q::CuArray{T,4},K::CuArray{T,4},V::CuArray{T,4}) where {T}
+    O = similar(Q)
     d, N, H, B = size(Q)
-    l = similar(Q, N, H, B)
-    m = similar(l)
-    Br =s
+
+    Bs = min(64, N) # block size
+    threads = (Bs, 1, 1)
+    blocks = (cld(N, Bs), H, B)
+    shmem = compute_shmem_size(d, Bs, T)
+    CUDA.@sync begin
+        @cuda threads=threads blocks=blocks shmem=shmem flash_attention_kernel(Q, K, V, O)
+    end
+    return O
+end
+
+@inline function compute_shmem_size(d, Bs, T)
+    return (d * Bs * 3 + Bs * 3 + Bs * Bs * 2) * sizeof(T)
 end
 
 # sanity check
 function ref_attention(Q,K,V)
-    KQ = batched_mul(permutedims(K, (2,1,3,4)), Q)
-    #P = softmax(KQ; dims=1)
-    P = KQ
+    S = batched_mul(permutedims(K, (2,1,3,4)), Q)
+    P = softmax(S)
     O = batched_mul(V, P)
     return O
 end
