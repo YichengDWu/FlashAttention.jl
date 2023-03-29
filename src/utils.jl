1fa85429fb37e80e3740d4eafa3ab5d84c386c06
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-03-29 00:59:26 -0600

    format

--- src/utils.jl
+++ src/utils.jl
@@ -9,5 +9,7 @@ Set the maximum shared memory size for the current device to `shmem` KB.
 """
 function setMaxShmem(shmem)
     kernel = cufunction(flash_attention_kernel, NTuple{4, CuDeviceArray{Float16, 4, 1}})
-    CUDA.cuFuncSetAttribute(kernel.fun, CUDA.CU_FUNC_ATTRIBUTE_MAX_DYNAMIC_SHARED_SIZE_BYTES, shmem*1024);
+    return CUDA.cuFuncSetAttribute(kernel.fun,
+                                   CUDA.CU_FUNC_ATTRIBUTE_MAX_DYNAMIC_SHARED_SIZE_BYTES,
+                                   shmem * 1024)
 end
