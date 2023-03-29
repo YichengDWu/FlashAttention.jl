f31b9643bdf4fc5c2e69e6ef99281f67229cd14d
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-03-28 21:14:27 -0600

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,9 +14,9 @@ end
 
 if CUDA.functional()
     @testset "FlashAttention.jl" begin
-        Q = CUDA.cu(rand(3, 255, 4, 3))
-        K = CUDA.cu(rand(3, 255, 4, 3))
-        V = CUDA.cu(rand(3, 255, 4, 3))
+        Q = CUDA.rand(Float16, 3, 255, 4, 3)
+        K = CUDA.rand(Float16, 3, 255, 4, 3)
+        V = CUDA.rand(Float16, 3, 255, 4, 3)
         O = flash_attention(Q, K, V)
         O_ref = ref_attention(Q, K, V)
         @test O ≈ O_ref
