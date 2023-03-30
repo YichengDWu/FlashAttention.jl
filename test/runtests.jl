a5514a1dd21987b257f43e2f77a9ba93f213dddf
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-03-29 20:35:32 -0600

    correct testing!

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,7 @@ function ref_attention(Q, K, V)
     sq, sk = size(Q), size(K)
     Q = reshape(Q, sq[1], sq[2], sq[3] * sq[4])
     K = reshape(K, sk[1], sk[2], sk[3] * sk[4])
-    S = CUBLAS.gemm_strided_batched('T', 'N', Q, K)
+    S = CUBLAS.gemm_strided_batched('T', 'N', K, Q)
     S = reshape(S, sk[2], sq[2], sq[3], sq[4])
     P = softmax(S)
     O = batched_mul(V, P)
@@ -22,14 +22,14 @@ if CUDA.functional()
         K = CUDA.rand(Float16, 3, 255, 4, 3)
         V = CUDA.rand(Float16, 3, 255, 4, 3)
         O = flash_attention(Q, K, V)
-        O_ref = ref_attention(Float32.(Q), Float32.(K), Float32.(V))
+        O_ref = ref_attention(Q, K, V)
         @test O ≈ O_ref
 
         Q = CUDA.rand(Float16, 64, 255, 4, 3)
         K = CUDA.rand(Float16, 64, 255, 4, 3)
         V = CUDA.rand(Float16, 64, 255, 4, 3)
         O = flash_attention(Q, K, V)
-        O_ref = ref_attention(Float32.(Q), Float32.(K), Float32.(V))
-        @test O ≈ O_ref rtol=1f-3
+        O_ref = ref_attention(Q, K, V)
+        @test O ≈ O_ref
     end
 end
